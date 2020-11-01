979df049f97a49e2ca84ca113bfd0eb9a8410c52
Miguel García Reimers <miguelgarciareimers@Miguels-MacBook-Pro.local>
2020-11-01 21:41:32 +0100

    User REST endpoints developed.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,42 @@
 const express = require('express');
 const app = express();
+
+app.use(express.json());
  
 app.get('/', function (req, res) {
   res.json('Hello World');
-})
+});
+
+app.get('/user', function (req, res) {
+  res.json('Get user');
+});
  
+app.post('/user', function (req, res) {
+  const { user } = req.body;
+
+  if (user === undefined || user === null || user.name === undefined || user.name === null) {
+    res.status(400).json({
+      ok: false,
+      message: 'Wrong data',
+    });
+
+    return;
+  }
+
+  res.json(user);
+});
+
+app.put('/user/:id', function (req, res) {
+  const { id } = req.params;
+
+  res.json({
+    id: id,
+    name: 'Miguel',
+  });
+});
+
+app.delete('/user', function (req, res) {
+  res.json('Delete user');
+});
+
 app.listen(3000, () => console.log('Listening port:', 3000));
