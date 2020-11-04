4de39997721d4dfff75b1c1d7a5695d0142ba864
Miguel García Reimers <miguelgarciareimers@Miguels-MacBook-Pro.local>
2020-11-04 12:22:17 +0100

    Environment DATABASE_URL in production changed.

--- server/config/config.js
+++ server/config/config.js
@@ -11,4 +11,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ===================
 // Database url
 // ===================
-process.env.DATABASE_URL = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/coffee' : 'mongodb+srv://mgarciareimers:3zgKWRqFipeG6goo@cluster0.n16ti.mongodb.net/coffee';
+process.env.DATABASE_URL = process.env.NODE_ENV === 'dev' ? 'mongodb://localhost:27017/coffee' : process.env.DATABASE_URL;
